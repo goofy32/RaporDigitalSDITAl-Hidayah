baa836398309921567d7c29fbdd9732b6bbcdf17
Goofy32 <tahrir2003@gmail.com>
2025-05-10 20:35:32 +0700

    history v1

--- resources/views/admin/report/history.blade.php
+++ resources/views/admin/report/history.blade.php
@@ -198,12 +198,6 @@
         
         <!-- Footer -->
         <div class="mt-6 flex justify-end space-x-3">
-            <button id="printPreviewBtn" onclick="printPreview()" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-                <svg class="inline-block w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"/>
-                </svg>
-                Print
-            </button>
             <button onclick="closePreviewModal()" class="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300">
                 Tutup
             </button>
